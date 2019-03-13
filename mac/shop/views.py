53adcf509001aeb67dcff2cd8081300dfaaef9fd
Amitojasa <amitojvmc@gmail.com>
2019-03-13 22:40:49 +0530

    work under progress

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -6,12 +6,19 @@ from math import ceil
 def index(request):
     products = product.objects.all()
     # print(products)
-    n = len(products)
-    nSlides = n//4 + ceil( n/4 - (n//4))
+
     # params = {'nSlides':nSlides,'product':products,'range':range(1,nSlides)}
-    allProds=[[products,range(1,nSlides),nSlides],
-              [products, range(1, nSlides), nSlides]
-              ]
+    # allProds=[[products,range(1,nSlides),nSlides],
+    #           [products, range(1, nSlides), nSlides]
+    #           ]
+    allProds = []
+    catProds = product.objects.values('category','id')
+    cats={item['category'] for item in catProds}
+    for cat in cats:
+        prod = product.objects.filter(category = cat)
+        n = len(products)
+        nSlides = n // 4 + ceil(n / 4 - (n // 4))
+        allProds.append([prod,range(1,nSlides),nSlides])
     params={'allProds':allProds}
     return render(request,'shop/index.html',params)
 
